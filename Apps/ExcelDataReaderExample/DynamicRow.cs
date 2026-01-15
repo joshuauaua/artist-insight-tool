58338e7afe9d392394e699c7bbf0efee058269f0
joshuauaua <joshjoelng@gmail.com>
2026-01-15 10:30:47 +0100

    Refactor
    Moved Dynamic Row and Set Value classes to separate files for cleaner code structure

---
+++ Apps/ExcelDataReaderExample/DynamicRow.cs
@@ -0,0 +1,112 @@
+namespace ExcelDataReaderExample;
+
+public class DynamicRow
+{
+  public object? Col0 { get; set; }
+  public object? Col1 { get; set; }
+  public object? Col2 { get; set; }
+  public object? Col3 { get; set; }
+  public object? Col4 { get; set; }
+  public object? Col5 { get; set; }
+  public object? Col6 { get; set; }
+  public object? Col7 { get; set; }
+  public object? Col8 { get; set; }
+  public object? Col9 { get; set; }
+  public object? Col10 { get; set; }
+  public object? Col11 { get; set; }
+  public object? Col12 { get; set; }
+  public object? Col13 { get; set; }
+  public object? Col14 { get; set; }
+  public object? Col15 { get; set; }
+  public object? Col16 { get; set; }
+  public object? Col17 { get; set; }
+  public object? Col18 { get; set; }
+  public object? Col19 { get; set; }
+  public object? Col20 { get; set; }
+  public object? Col21 { get; set; }
+  public object? Col22 { get; set; }
+  public object? Col23 { get; set; }
+  public object? Col24 { get; set; }
+  public object? Col25 { get; set; }
+  public object? Col26 { get; set; }
+  public object? Col27 { get; set; }
+  public object? Col28 { get; set; }
+  public object? Col29 { get; set; }
+  public object? Col30 { get; set; }
+  public object? Col31 { get; set; }
+  public object? Col32 { get; set; }
+  public object? Col33 { get; set; }
+  public object? Col34 { get; set; }
+  public object? Col35 { get; set; }
+  public object? Col36 { get; set; }
+  public object? Col37 { get; set; }
+  public object? Col38 { get; set; }
+  public object? Col39 { get; set; }
+  public object? Col40 { get; set; }
+  public object? Col41 { get; set; }
+  public object? Col42 { get; set; }
+  public object? Col43 { get; set; }
+  public object? Col44 { get; set; }
+  public object? Col45 { get; set; }
+  public object? Col46 { get; set; }
+  public object? Col47 { get; set; }
+  public object? Col48 { get; set; }
+  public object? Col49 { get; set; }
+
+  public void SetValue(int index, object? value)
+  {
+    switch (index)
+    {
+      case 0: Col0 = value; break;
+      case 1: Col1 = value; break;
+      case 2: Col2 = value; break;
+      case 3: Col3 = value; break;
+      case 4: Col4 = value; break;
+      case 5: Col5 = value; break;
+      case 6: Col6 = value; break;
+      case 7: Col7 = value; break;
+      case 8: Col8 = value; break;
+      case 9: Col9 = value; break;
+      case 10: Col10 = value; break;
+      case 11: Col11 = value; break;
+      case 12: Col12 = value; break;
+      case 13: Col13 = value; break;
+      case 14: Col14 = value; break;
+      case 15: Col15 = value; break;
+      case 16: Col16 = value; break;
+      case 17: Col17 = value; break;
+      case 18: Col18 = value; break;
+      case 19: Col19 = value; break;
+      case 20: Col20 = value; break;
+      case 21: Col21 = value; break;
+      case 22: Col22 = value; break;
+      case 23: Col23 = value; break;
+      case 24: Col24 = value; break;
+      case 25: Col25 = value; break;
+      case 26: Col26 = value; break;
+      case 27: Col27 = value; break;
+      case 28: Col28 = value; break;
+      case 29: Col29 = value; break;
+      case 30: Col30 = value; break;
+      case 31: Col31 = value; break;
+      case 32: Col32 = value; break;
+      case 33: Col33 = value; break;
+      case 34: Col34 = value; break;
+      case 35: Col35 = value; break;
+      case 36: Col36 = value; break;
+      case 37: Col37 = value; break;
+      case 38: Col38 = value; break;
+      case 39: Col39 = value; break;
+      case 40: Col40 = value; break;
+      case 41: Col41 = value; break;
+      case 42: Col42 = value; break;
+      case 43: Col43 = value; break;
+      case 44: Col44 = value; break;
+      case 45: Col45 = value; break;
+      case 46: Col46 = value; break;
+      case 47: Col47 = value; break;
+      case 48: Col48 = value; break;
+      case 49: Col49 = value; break;
+    }
+  }
+}
