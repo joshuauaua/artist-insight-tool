a9209c08cec6b80325fef598e429b4f269649ced
joshuauaua <joshjoelng@gmail.com>
2026-01-17 15:57:03 +0100

    Fix: updated schema with EF

---
+++ Migrations/20260117145635_AddAssetCategoryAndCollection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtistInsightTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssetCategoryAndCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "assets",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Collection",
+                table: "assets",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "assets");
+
+            migrationBuilder.DropColumn(
+                name: "Collection",
+                table: "assets");
+        }
+    }
+}
